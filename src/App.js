fec7aa210d5eb2bb9cf51618c811a60c83c7a48c
user <ssharp0614@gmail.com>
2025-10-16 16:57:25 +0900

    feat(app): handle empty input in calculator and display result

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async calculator() {
     const STR = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n')
+
+    if (STR === '')
+      return 0
   }
 
   async run() {
     try {
-      await this.calculator()
+      Console.print('결과 : ' + await this.calculator())
     } catch (error) {
       Console.print(`[ERROR] ${error.message}`)
     }
