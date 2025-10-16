e574df2e656c438581f03e4fcaec862fdbb2e9cc
user <ssharp0614@gmail.com>
2025-10-16 17:29:48 +0900

    feat(app): Add delimiters array and support custom delimiters

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class App {
+  extractCustomDelimiter(str) {
+    if (!str.startsWith('//')) return null
+    return str.split('\\n')[0].split('//')[1]
+  }
+
   async calculator() {
     const STR = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n')
 
     if (STR === '')
       return 0
+
+    const DELIMITERS = [',', ':']
+
+    if (this.extractCustomDelimiter(STR))
+      DELIMITERS.push(this.extractCustomDelimiter(STR))
   }
 
   async run() {
