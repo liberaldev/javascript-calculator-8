6c4166aafa933740412cc0d787ad64c5ac565561
user <ssharp0614@gmail.com>
2025-10-16 16:24:19 +0900

    feat(app): add Console input handling with error management

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async calculator() {
+    const STR = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n')
+  }
+
+  async run() {
+    try {
+      await this.calculator()
+    } catch (error) {
+      Console.print(`[ERROR] ${error.message}`)
+    }
+  }
 }
 
 export default App;
