4076e8dfba1c6a6fdffe010b54abaaa2deab4eef
user <ssharp0614@gmail.com>
2025-10-20 23:36:01 +0900

    style(app): apply consistent code style and improve readability

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console } from '@woowacourse/mission-utils';
 
 class App {
-  #extractCustomDelimiter (str) {
+  static #extractCustomDelimiter(str) {
     if (!str.startsWith('//')) return null;
     return str.split('\\n')[0].split('//')[1];
   }
 
   async #calculator(str) {
-    if (str === '')
+    if (str === '') {
       return 0;
+    }
 
     const DELIMITERS = [',', ':'];
 
     let numbers;
 
     if (this.#extractCustomDelimiter(str)) {
-      DELIMITERS.push(this.#extractCustomDelimiter(str))
+      DELIMITERS.push(this.#extractCustomDelimiter(str));
       numbers = str.split('\\n')[1].split(new RegExp(`[${DELIMITERS.join('')}]`));
     } else {
       numbers = str.split(new RegExp(`[${DELIMITERS.join('')}]`));
     }
 
-    if (numbers.find(num => Number.isNaN(Number(num)))) {
+    if (numbers.find((num) => Number.isNaN(Number(num)))) {
       throw new Error('[ERROR] 숫자가 아닌 문자가 포함되어 있습니다');
     }
 
-    if (numbers.find(num => num < 0)) {
+    if (numbers.find((num) => num < 0)) {
       throw new Error('[ERROR] 양수가 아닌 숫자가 포함되어 있습니다');
     }
 
@@ -34,7 +35,7 @@ class App {
 
   async run() {
     const STR = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
-    Console.print('결과 : ' + await this.#calculator(STR));
+    Console.print(`결과 : ${await this.#calculator(STR)}`);
   }
 }
 
